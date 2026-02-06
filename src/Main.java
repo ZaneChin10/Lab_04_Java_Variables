30bf17179aebf842c1e1b90688b23dd02b5a4a91
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-02-06 09:47:45 -0500

    Commit 01

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,19 @@ public class Main {
         public static void main(String[] args) {
 
 
-            
+            int intOperandA = 56;
+            int intOperandB = 12;
+            int intSum = intOperandA + intOperandB;
+            int intDifference = intOperandA - intOperandB;
+            int intProduct = intOperandA * intOperandB;
+            int intModulo = intOperandA % intOperandB;
+            int intQuotient = intOperandA / intOperandB;
+
+            System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+            System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+            System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+            System.out.println("The remainder of " + intOperandA + " divided by " + intOperandB + " is " + intModulo);
+            System.out.println("The quotient of " + intOperandA + " divided by " + intOperandB + " is " + intQuotient);
         }
 
 }
