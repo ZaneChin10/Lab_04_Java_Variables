f795981a1e063b649f26b85eba875875bb76725f
Zane Sawyer <sawyerzc@mail.uc.edu>
2026-02-07 11:28:26 -0500

    Commit 02

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,34 @@ public class Main {
             int intProduct = intOperandA * intOperandB;
             int intModulo = intOperandA % intOperandB;
             int intQuotient = intOperandA / intOperandB;
+            double doubleOperandA = 56.5;
+            double doubleOperandB = 12.5;
+            double doubleSum = doubleOperandA + doubleOperandB;
+            double doubleDifference = doubleOperandA - doubleOperandB;
+            double doubleProduct = doubleOperandA * doubleOperandB;
+            double doubleModulo = doubleOperandA % doubleOperandB;
+            double doubleQuotient = doubleOperandA / doubleOperandB;
+            int numKidsInFamily = 3;
+            boolean isItRaining = true;
+            double gasPricePerGallon = 2.79;
+            int yourFavNumber = 12;
+            double yourShoeSize = 10.5;
+            String yourBirthMonth = "March";
+            String yourFullName = "Zane Chin";
+
 
             System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
             System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
             System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
             System.out.println("The remainder of " + intOperandA + " divided by " + intOperandB + " is " + intModulo);
             System.out.println("The quotient of " + intOperandA + " divided by " + intOperandB + " is " + intQuotient);
+            System.out.println("The sum using Doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+            System.out.println("The difference using Doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+            System.out.println("The product using Doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+            System.out.println("The remainder using Doubles of " + doubleOperandA + " divided by " + doubleOperandB + " is " + doubleModulo);
+            System.out.println("The quotient using Doubles of " + doubleOperandA + " divided by " + doubleOperandB + " is " + doubleQuotient);
+
         }
+
 
 }
